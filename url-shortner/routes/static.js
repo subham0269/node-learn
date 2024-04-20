3b7022b0cb1cec94ccc03a2e82e5846fece8751d
subham0269 <gitsubham21@gmail.com>
2024-04-20 13:16:24 +0530

    Add all url link table to home route

--- url-shortner/routes/static.js
+++ url-shortner/routes/static.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import urlModel from '../models/url.js';
 
 const router = express.Router();
 
-router.route('/').get((req,res) => {
-  return res.render('home');
+router.route('/').get(async (req,res) => {
+  try {
+    const allUrls = await urlModel.find({})
+    return res.render('home', {
+      urls: allUrls
+    });
+  } catch (err) {
+    return res.status(500).json({error : 'Internal Server Error'})
+  }
 })
 
 router.route('/signup').get((req,res) => {
